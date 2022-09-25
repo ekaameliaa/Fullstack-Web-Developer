4c2d873febd88a94d4104001098ec947fb2abd9d
ekaameliaa <72693330+ekaameliaa@users.noreply.github.com>
2022-09-25 18:55:07 +0700

    tugas7

---
+++ Tugas7JS_EkaAmeliaPutri_STTNF/js/style.js
@@ -0,0 +1,87 @@
+
+$("document").ready(function () {
+    $("button.ayam").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/ayam.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.bebek").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/bebek.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.burung").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/burung.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.penguin").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/penguin.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.kambing").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/kambing.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.sapi").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/sapi.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.kelinci").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/kelinci.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.kerbau").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/kerbau.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.harimau").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/harimau.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.cheetah").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/Cheetah.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.badak").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/badak.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+    $("button.singa").click(function () {
+        $("<audio></audio>").attr({
+            'src': 'audio/singa.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("div");
+    });
+});
